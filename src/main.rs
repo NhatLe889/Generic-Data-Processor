314062feaa9544d341ce089e833c2dc776bf1ec8
Nhat Le <nhatle@Nhats-MacBook-Pro-2.local>
2025-10-14 19:52:08 -0400

    fixed sort issue with multiple digit by implementing sort that first convert to number

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ mod processor;
 use crate::data::*;
 use crate::processor::*;
 
+enum MixedType {
+    Integer(i32),
+    Text(String),
+    Identity(Person),
+}
+
 fn main() {
     let numbers = vec![3, 2, 1, 5, 9, 18, 25];
     let words = vec!["apple".to_string(), "pear".to_string(), "banana".to_string(), "grape".to_string()];
@@ -15,6 +21,14 @@ fn main() {
         Person::new("Josh", 99),
         Person::new("George", 25),
     ];
+
+    let unicorn_test = vec![
+        MixedType::Integer(18),
+        MixedType::Integer(3),
+        MixedType::Identity(Person::new("Lebron", 40)),
+        MixedType::Text("Strawberry".to_string()),
+    ];
+    //Implemnt sort for mixedtype later.
 
     //Using Generic Processor
     println!("Sorted numbers: {:?}", process_data(numbers));
--- src/processor.rs
+++ src/processor.rs
@@ -4,6 +4,17 @@ pub fn process_data<T>(mut data: Vec<T>) -> Vec<T>
 where
     T: Processable + Ord + Clone,
 {
-    data.sort_by_key(|item| item.key());
+    data.sort_by(|a, b| {
+        let ka = a.key();
+        let kb = b.key();
+
+        let na = ka.parse::<f64>();
+        let nb = kb.parse::<f64>();
+
+        match (na, nb) {
+            (Ok(na), Ok(nb)) => na.partial_cmp(&nb).unwrap(),
+            _ => ka.cmp(&kb),
+        }
+    });
     data
 }
